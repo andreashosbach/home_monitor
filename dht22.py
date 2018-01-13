033119a0e4b4c371407e706ad03716a9af39b7cb
andreashosbach <andreas.hosbach@gmail.com>
2018-01-13 22:32:41 +0100

    dht22 error handling

--- dht22.py
+++ dht22.py
@@ -5,4 +5,7 @@ import Adafruit_DHT
 # =============================================================================
 def read_sensor(sensor_pin): 
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, sensor_pin) 
+    if humidity is None and temperature is None:
+        print("Failed to get reading on sensor: " + str(sensor_pin))
+
     return (humidity, temperature) 
